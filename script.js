3dff6d1cc7ef658485c16a30df3880d1f1e20861
ZUZ <megazuz@gmail.com>
2024-06-03 02:01:27 +0300

    Finish basic exercise in lesson02

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-let title,
-    screens,
-    screenPrice,
-    rollback,
-    fullPrice,
-    adaptive;
+let title = 'GLO-course-JS',
+    screens = 'Простые, Сложные, Интерактивные',
+    screenPrice = 4,
+    rollback = 23,
+    fullPrice = 100500,
+    adaptive = true;
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани');
+console.log('Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани');
+
+console.log(screens.toLowerCase().split(', '));
+let cost = fullPrice * (rollback / 100)
+console.log('Процент отката посреднику за работу ' + cost.toString() + ' рублей/долларов/гривен/юани');
 
 alert('File script.js successfully loaded');
 console.log('File script.js successfully loaded');
